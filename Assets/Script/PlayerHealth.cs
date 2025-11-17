52feca1e673461f409b42fb28c6c4c0ee7144bce
BL4NK-USER <202480108@psu.palawan.edu.ph>
2025-11-17 18:37:10 +0800

    HealthTextADD

--- Assets/Script/PlayerHealth.cs
+++ Assets/Script/PlayerHealth.cs
@@ -1,14 +1,26 @@
 using UnityEngine;
+using TMPro;
 
 public class PlayerHealth : MonoBehaviour
 {
 
     public int MaximumHealth;
     public int CurrentHealth;
+    public TMP_Text healthtext;
+    public Animator Healthtextanim;
+
+    private void Start()
+    {
+        healthtext.text = "HP: " + CurrentHealth + " / " + MaximumHealth;
+    }
+
 
     public void ChangeHealth(int amount) 
     {
         CurrentHealth += amount;
+        Healthtextanim.Play("TextUpdate");
+
+        healthtext.text = "HP: " + CurrentHealth + " / " + MaximumHealth;
 
         if (CurrentHealth <= 0)
         {
